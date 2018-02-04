fc3feccd2f1dc3b17d69998cf45de3ffc344f359
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-02-04 19:47:47 +0300

    using response case class

--- src/main/scala/ru/tolsi/lykke/waves/wallet/routes/WalletsRoute.scala
+++ src/main/scala/ru/tolsi/lykke/waves/wallet/routes/WalletsRoute.scala
@@ -4,16 +4,20 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.wavesplatform.wavesj.{Base58, PrivateKeyAccount}
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
-import play.api.libs.json.{JsObject, JsString}
+import play.api.libs.json.{Json, Writes}
 import ru.tolsi.lykke.common.NetworkType
 import ru.tolsi.lykke.common.http.NetworkScheme
 
+object WalletsRoute {
+
+  case class ResponseObject(privateKey: String, publicAddress: String)
+
+  implicit val ResponseWrites: Writes[ResponseObject] = Json.writes[ResponseObject]
+}
+
 case class WalletsRoute(networkType: NetworkType) extends PlayJsonSupport with NetworkScheme {
 
-  private def responseObject(privateKey: String, publicAddress: String) =
-    JsObject(Map(
-      "privateKey" -> JsString(privateKey),
-      "publicAddress" -> JsString(publicAddress)))
+  import WalletsRoute._
 
   val route: Route = path("wallets") {
     post {
@@ -21,7 +25,7 @@ case class WalletsRoute(networkType: NetworkType) extends PlayJsonSupport with N
 
       val account = PrivateKeyAccount.fromSeed(randomSeed, 0, scheme)
 
-      complete(responseObject(Base58.encode(account.getPrivateKey), account.getAddress))
+      complete(ResponseObject(Base58.encode(account.getPrivateKey), account.getAddress))
     }
   }
 }
